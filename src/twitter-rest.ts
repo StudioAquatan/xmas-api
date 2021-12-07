02cbe9611a30cdb85ad16bc895d9fea29c858e91
f0reachARR <f0reach@f0reach.me>
2021-12-07 11:48:21 +0900

    add hashtag monitor keyword

--- src/twitter-rest.ts
+++ src/twitter-rest.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { config } from './config';
+import { HashtagMonitorModel } from './models/monitor';
 import { registerWebhook, subscribeWebhook } from './twitter-webhook-api';
 
 export const restAPIRouter = Router();
@@ -18,4 +19,36 @@ restAPIRouter.post('/subscribeWebhook', async (req, res) => {
   await subscribeWebhook(req.user, config.twitter.webhookEnv);
 
   res.status(200).end();
+});
+
+restAPIRouter.put('/monitorTweet', async (req, res) => {
+  if (!req.user) {
+    return res.status(401).end();
+  }
+
+  let hashtag = req.body.hashtag;
+  if (typeof hashtag !== 'string') {
+    return res.status(400).end();
+  }
+
+  if (hashtag.startsWith('#')) {
+    hashtag = hashtag.replace(/^#/, '');
+  }
+
+  const rule = await HashtagMonitorModel.findOne({ hashtag });
+  if (rule) {
+    return res.status(409).json({ error: 'Same rule found' });
+  }
+
+  await HashtagMonitorModel.insert({
+    hashtag,
+    count: 0,
+  });
+
+  const newRule = await HashtagMonitorModel.findOne({ hashtag });
+  res.json({
+    id: newRule?.id,
+    hashtag: newRule?.hashtag,
+    count: newRule?.count,
+  });
 });
