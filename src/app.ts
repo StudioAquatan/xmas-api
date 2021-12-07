d36c4710ab8976881e8b6fb07ef3cae018050c53
f0reachARR <f0reach@f0reach.me>
2021-12-07 09:22:00 +0900

    add session database

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { urlencoded, json } from 'body-parser';
+import { TypeormStore, ISession } from 'connect-typeorm';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import session from 'express-session';
@@ -12,6 +13,7 @@ import {
   HashtagTweet,
   TweetMonitorModel,
 } from './models/monitor';
+import { SessionModel } from './models/session';
 import { UserModel } from './models/user';
 
 passport.use(
@@ -50,25 +52,43 @@ passport.deserializeUser(async (id, done) => {
   done(null, user);
 });
 
-const app = express();
-
-app.use(cookieParser());
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(session({ secret: 'keyboard cat' }));
-app.use(morgan('combined'));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/api/twitter/login', passport.authenticate('twitter'));
-
 (async () => {
-  await createConnection({
-    entities: [UserModel, TweetMonitorModel, HashtagMonitorModel, HashtagTweet],
+  const connection = await createConnection({
+    entities: [
+      UserModel,
+      TweetMonitorModel,
+      HashtagMonitorModel,
+      HashtagTweet,
+      SessionModel,
+    ],
     type: 'sqlite',
     database: './data.db',
     synchronize: true,
   });
   console.log('Database connection established');
+
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(urlencoded({ extended: true }));
+  app.use(json());
+
+  const sessionRepository = connection.getRepository<ISession>(SessionModel);
+  app.use(
+    session({
+      secret: 'keyboard cat',
+      store: new TypeormStore({
+        cleanupLimit: 2,
+        limitSubquery: false, // If using MariaDB.
+        ttl: 86400,
+      }).connect(sessionRepository),
+    }),
+  );
+
+  app.use(morgan('combined'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.get('/api/twitter/login', passport.authenticate('twitter'));
   app.listen(3000, () => console.log('Http server started'));
 })();
